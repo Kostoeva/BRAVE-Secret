82b4daadd7a71c75ddbfcf4f72e686d4058a8b27
Kostoeva <irmareka@gmail.com>
2021-01-17 20:14:35 -0800

    Adding custom arcade asset and DLT assets

---
+++ unity/New Unity Project/Assets/Scripts/ButtonControl.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonControl : MonoBehaviour
+{
+    private bool on = false;
+    private bool buttonPressed = false;
+    public GameObject button;
+
+    private float buttonOriginalY;
+    private float buttonReturnSpeed = 0.001f;
+
+    private float buttonHitAgainTime = 0.5f;
+    private float canHitAgain;
+
+    private GameObject screen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttonOriginalY = button.transform.position.y;
+
+        screen = GameObject.Find("ArcadeMachineScreen");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (buttonPressed)
+        {
+
+            buttonPressed = false;
+            on = !on;
+
+            button.transform.position = new Vector3(button.transform.position.x, button.transform.position.y - 0.05f, button.transform.position.z);
+
+            if (on)
+            {
+                Debug.Log(screen.GetComponent<Renderer>().material.mainTexture.name.ToString());
+                SceneManager.LoadScene(screen.GetComponent<Renderer>().material.mainTexture.name.ToString());
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PlayerHand") && canHitAgain < Time.time)
+        {
+            canHitAgain = Time.time + buttonHitAgainTime;
+            buttonPressed = true;
+        }
+    }
+}
---
+++ unity/New Unity Project/Assets/Scripts/EventManagerDichoticListening.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EventManagerDichoticListening : MonoBehaviour
+{
+    public AudioSource IntroAudio;
+    public OVRCameraRig Player;
+    public GameObject speaker1;
+    public GameObject speaker2;
+    // TODO: Add levels
+    public GameObject lv1;
+    public GameObject lv2;
+    public GameObject lv3;
+    public GameObject lv4;
+    public bool gameEnds;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Intro());
+    }
+
+    IEnumerator Intro()
+    {
+        IntroAudio.Play();
+        yield return new WaitForSeconds(IntroAudio.clip.length); // wait time
+        //supposedly wait for player to set difficulty here
+        selectTarget();
+        //select difficulty
+        //play audio to introduce difficulty selection I guess
+
+        speaker1.SetActive(true);
+    }
+
+    private void selectDifficulty() {
+    }
+
+    private void selectTarget() {
+        System.Random rnd = new System.Random();
+        int target = rnd.Next(1, 21);  // creates a number between 1 and 20
+        SpeakerTarget script1 = speaker1.GetComponent<SpeakerTarget>();
+        script1.target = target;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameEnds) {
+            speaker1.SetActive(false);
+            StartCoroutine(Exit());
+        }
+    }
+    
+    IEnumerator Exit()
+    {
+        yield return new WaitForSeconds(5);
+        SceneManager.LoadScene("BRAVE");
+    }
+}
